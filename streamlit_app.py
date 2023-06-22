47b2b68c9f733cffcbc4a8613f3293c68c5d4064
samayam1713 <92843500+samayam1713@users.noreply.github.com>
2023-06-22 15:52:21 +0530

    Update streamlit_app.py
    Fixed Typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Bluebeery oatmeal')
+streamlit.text('kale, spinach & Rocket Smoothiee')
+typo.txt('Hot-Boiled Free-Range Egg')
